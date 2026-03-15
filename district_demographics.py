35507a683a2803acbe9de5690872a0aa2bd599bf
Derek Eder <derek.eder@gmail.com>
2026-03-14 23:18:23 -0500

    polish script, working

--- district_demographics.py
+++ district_demographics.py
@@ -16,36 +16,22 @@ with open('data/oak-park-municipal-boundary.geojson', 'r') as f:
 with open('data/oak-park-historic-districts.geojson', 'r') as f:
     districts = json.load(f)
 
-# ACS5 SEX BY AGE (B01001) SUBTABLE DEFINITIONS
-#
-# RACE/ETHNICITY
-# B - Black only
-# H - White, not Hispanic
-# I - Hispanic
-#
-# SEX/AGE
-# 005 - Boys, ages 10-14
-# 006 - Boys, ages 15-17
-# 020 - Girls, ages 10-14
-# 021 - Girls, ages 15-17
-
 table_map = {
-    'black_male': ['B01001B_005E', 'B01001B_006E'],
-    'black_female': ['B01001B_020E', 'B01001B_021E'],
-    'white_male': ['B01001H_005E', 'B01001H_006E'],
-    'white_female': ['B01001H_020E', 'B01001H_021E'],
-    'hispanic_male': ['B01001I_005E', 'B01001I_006E'],
-    'hispanic_female': ['B01001I_020E', 'B01001I_021E']
+    'black': ['P1_004N'],
+    'hispanic': ['P2_002N'],
+    'white': ['P4_005N'],
+    'asian': ['P1_006N'],
+    'two+': ['P1_009N'],
+    'total': ['P1_001N']
 }
 
 header = ['Area'] + sorted(table_map.keys())
 
 writer.writerow(header)
 
-# all_areas = districts['features'].append(oak_park['features'])
-# print(all_areas)
+all_areas = districts['features'] + oak_park['features']
 
-for area in districts['features']:
+for area in all_areas:
     name = area['properties']['NAME']
 
     # get populations
@@ -54,9 +40,9 @@ for area in districts['features']:
     for demographic, tables in table_map.items():
         population = 0
         for table in tables:
-            acs_data = c.acs5.geo_tract(('NAME', table), area['geometry'])
-            for _, tract_data in acs_data:
-                population += int(tract_data[table])
+            census_data = c.pl.geo_block(('NAME', table), area['geometry'], 2020)
+            for geo, vals, overlap in census_data:
+                population += int(vals[table])
         population_obj.update({demographic: population})
 
     # write to csv
